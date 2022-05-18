0089cc1cf78e74627402cb6ce417c7127e4dbf10
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-05-17 21:34:02 -0600

    CRUD comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,20 +4,37 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then((dbPizzaData) => res.json(dbPizzaData))
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
       .catch((err) => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
@@ -25,7 +42,7 @@ const pizzaController = {
   createPizza({ body }, res) {
     Pizza.create(body)
       .then((dbPizzaData) => res.json(dbPizzaData))
-      .catch((err) => res.json(err));
+      .catch((err) => res.status(400).json(err));
   },
 
   // update pizza by id
