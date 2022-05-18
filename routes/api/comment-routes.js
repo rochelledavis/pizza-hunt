ca2510dd8da3cbd3ec0da7e2804f9e39810bbe92
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-05-17 22:43:11 -0600

    replies functionality complete

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,18 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
 router.route("/:pizzaId").post(addComment);
 
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
 // /api/comments/<pizzaId>/<commentId>
 router.route("/:pizzaId/:commentId").delete(removeComment);
+
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
